508c20448060cffad1ecee99098e0e729bc27e86
Fabian Meyer <fabian.meyer1337@web.de>
2017-06-22 22:45:56 +0200

    first derivative added

--- assignment/5/582fixedPointProblem.m
+++ assignment/5/582fixedPointProblem.m
@@ -4,9 +4,15 @@ f = @(x) (2*x - tan(x));
 
 #g is a fixed point problem for f and global convergent on [1,1.5]
 g = @(x) (atan(2*x));
+#first derivative (monotonically decreasing) so L is at x=1, g'(1)= 0.4 = L
+# L can be used for aposterio and apriori estim.
+gd = @(x) (2./(4*x.^2+1));
+
 
 #h is the fixed point problem by newtons method 
 h = @(x) ((-x*sec(x)*sec(x) +tan(x))/(2-sec(x)*sec(x)));
+
+
 
 #iterate to find zero crossing
 ng = iterate (g,10,1.2);
