db216a1e6137ba0633fb210477092e58fd827a74
Fabian Meyer <fabian.meyer1337@web.de>
2017-06-22 19:12:25 +0200

    first commit, only for backup purposes

---
+++ assignment/5/582fixedPointProblem.m
@@ -0,0 +1,22 @@
+source "../../general/simpleFunctions.m"
+#f is the function where we look for a zero crossing between [1,1.5]
+f = @(x) (2*x - tan(x));
+
+#g is a fixed point problem for f and global convergent on [1,1.5]
+g = @(x) (atan(2*x));
+
+#h is the fixed point problem by newtons method 
+h = @(x) ((-x*sec(x)*sec(x) +tan(x))/(2-sec(x)*sec(x)));
+
+#iterate to find zero crossing
+ng = iterate (g,10,1.2);
+nh = iterate (h,10,1.2);
+
+errorOf_g = f(ng)
+errorOf_h = f(nh)
+
+#h therefore has quadratic convergence (due to newton)
+#whereas g has only linear convergence
+
+y = [1:0.01:1.5];
+plot(y,f(y),y,g(y),ng,f(ng),nh,f(nh))
---
+++ general/simpleFunctions.m
@@ -0,0 +1,6 @@
+# apply function f n times => f(f(...f(x)...))
+function x = iterate(func,n,x)
+  for i=1:n
+  x = func(x);
+  end
+end
