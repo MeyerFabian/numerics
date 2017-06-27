4c71051c503039f7c8e6d7f47eb35649f97744bc
Fabian Meyer <fabian.meyer1337@web.de>
2017-06-27 19:16:20 +0200

    comments

--- general/gaussnewton.m
+++ general/gaussnewton.m
@@ -1,9 +1,11 @@
 
-#newton method for arbitrary dimensions
+#gaussnewton method for arbitrary dimensions
 #f : function of which to find a minimum of dimension (m x 1)
-#x : starting value of the fixpoint iteration (n x 1)
+#x : starting parameters of the fixpoint iteration (n x 1)
 #fd: the jacobian matrix of f of dimension (m x n)
 #k : the number of iterations
+#gradF : gradient of f
+#sHF: is the simplified Hessian ignoring squared terms
 function x = gaussnewton(x,f,fd,k,varargin)
   for(it=1:k)
     F = evalFMatrix(f,x,varargin{:});
