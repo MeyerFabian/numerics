18a5ece6e57372d441349b3fc374200b706d88d5
Fabian Meyer <fabian.meyer1337@web.de>
2017-07-07 17:14:31 +0200

    fixed

--- assignment/8/splinesExample840.m
+++ assignment/8/splinesExample840.m
@@ -7,13 +7,13 @@ y = [2.5 2.0 0.5 0.5 1.5 1.0 1.125 0.0];
 m = splineCreation(x,y,h);
 
 n = length(x)-1;
-plot(x,f,'o');
+plot(x,y,'o');
 hold on;
 for(i=1:n)
   sx = [x(i):(h*0.1):x(i+1)];
   sy = spline(sx,x(i),x(i+1),y(i),y(i+1),m(i),m(i+1),h);
   plot(sx,sy);
 end
-
+hold off;
 
 
